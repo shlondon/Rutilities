f46b32dd2bdc8a88be5573957743d2cc5fa0b7a4
Santiago Humberto Londoño Restrepo <s.londono@getaclub.io>
2022-08-19 19:46:33 -0500

    It is running demo but there is an error in building initial model step

--- RobynHW/poe_project/robyn-demo/demo.R
+++ RobynHW/poe_project/robyn-demo/demo.R
@@ -6,7 +6,8 @@ library(Robyn)
 
 # Usar ambiente virtual donde esta la libreria de python nevergrad
 library(reticulate)
-use_virtualenv("/Users/santiagolondono/Documents/Proyectos/Rutilities/RobynHW/poe_project/venv", required = TRUE)
+# use_virtualenv("/Users/santiagolondono/Documents/Proyectos/Rutilities/RobynHW/poe_project/venv", required = TRUE)
+use_virtualenv("/Users/santiagolondono/Documents/Proyectos/Rutilities/RobynHW/poe_project/robyn-demo/venv", required = TRUE)
 
 #### Step 1: Load data
 ## Check simulated dataset or load your own dataset
@@ -68,4 +69,149 @@ InputCollect <- robyn_inputs(
 )
 print(InputCollect)
 
-?robyn_inputs
+#### 2a-2: Second, define and add hyperparameters
+
+## -------------------------------- NOTE v3.6.0 CHANGE !!! ---------------------------------- ##
+## Default media variable for modelling has changed from paid_media_vars to paid_media_spends.
+## hyperparameter names needs to be base on paid_media_spends names. Run:
+hyper_names(adstock = InputCollect$adstock, all_media = InputCollect$all_media)
+## to see correct hyperparameter names. Check GitHub homepage for background of change.
+## Also calibration_input are required to be spend names.
+## ------------------------------------------------------------------------------------------ ##
+
+## Guide to setup & understand hyperparameters
+
+## 1. IMPORTANT: set plot = TRUE to see helper plots of hyperparameter's effect in transformation
+plot_adstock(plot = FALSE)
+plot_saturation(plot = FALSE)
+
+## 2. Get correct hyperparameter names:
+# All variables in paid_media_spends and organic_vars require hyperparameter and will be
+# transformed by adstock & saturation.
+# Run hyper_names() as above to get correct media hyperparameter names. All names in
+# hyperparameters must equal names from hyper_names(), case sensitive.
+# Run ?hyper_names to check parameter definition.
+
+## 3. Hyperparameter interpretation & recommendation:
+
+## Geometric adstock: Theta is the only parameter and means fixed decay rate. Assuming TV
+# spend on day 1 is 100€ and theta = 0.7, then day 2 has 100*0.7=70€ worth of effect
+# carried-over from day 1, day 3 has 70*0.7=49€ from day 2 etc. Rule-of-thumb for common
+# media genre: TV c(0.3, 0.8), OOH/Print/Radio c(0.1, 0.4), digital c(0, 0.3)
+
+## Weibull CDF adstock: The Cumulative Distribution Function of Weibull has two parameters
+# , shape & scale, and has flexible decay rate, compared to Geometric adstock with fixed
+# decay rate. The shape parameter controls the shape of the decay curve. Recommended
+# bound is c(0.0001, 2). The larger the shape, the more S-shape. The smaller, the more
+# L-shape. Scale controls the inflexion point of the decay curve. We recommend very
+# conservative bounce of c(0, 0.1), because scale increases the adstock half-life greatly.
+
+## Weibull PDF adstock: The Probability Density Function of the Weibull also has two
+# parameters, shape & scale, and also has flexible decay rate as Weibull CDF. The
+# difference is that Weibull PDF offers lagged effect. When shape > 2, the curve peaks
+# after x = 0 and has NULL slope at x = 0, enabling lagged effect and sharper increase and
+# decrease of adstock, while the scale parameter indicates the limit of the relative
+# position of the peak at x axis; when 1 < shape < 2, the curve peaks after x = 0 and has
+# infinite positive slope at x = 0, enabling lagged effect and slower increase and decrease
+# of adstock, while scale has the same effect as above; when shape = 1, the curve peaks at
+# x = 0 and reduces to exponential decay, while scale controls the inflexion point; when
+# 0 < shape < 1, the curve peaks at x = 0 and has increasing decay, while scale controls
+# the inflexion point. When all possible shapes are relevant, we recommend c(0.0001, 10)
+# as bounds for shape; when only strong lagged effect is of interest, we recommend
+# c(2.0001, 10) as bound for shape. In all cases, we recommend conservative bound of
+# c(0, 0.1) for scale. Due to the great flexibility of Weibull PDF, meaning more freedom
+# in hyperparameter spaces for Nevergrad to explore, it also requires larger iterations
+# to converge.
+
+## Hill function for saturation: Hill function is a two-parametric function in Robyn with
+# alpha and gamma. Alpha controls the shape of the curve between exponential and s-shape.
+# Recommended bound is c(0.5, 3). The larger the alpha, the more S-shape. The smaller, the
+# more C-shape. Gamma controls the inflexion point. Recommended bounce is c(0.3, 1). The
+# larger the gamma, the later the inflection point in the response curve.
+
+## 4. Set individual hyperparameter bounds. They either contain two values e.g. c(0, 0.5),
+# or only one value, in which case you'd "fix" that hyperparameter.
+
+# Run hyper_limits() to check maximum upper and lower bounds by range
+# Example hyperparameters ranges for Geometric adstock
+hyperparameters <- list(
+  facebook_S_alphas = c(0.5, 3),
+  facebook_S_gammas = c(0.3, 1),
+  facebook_S_thetas = c(0, 0.3),
+  print_S_alphas = c(0.5, 3),
+  print_S_gammas = c(0.3, 1),
+  print_S_thetas = c(0.1, 0.4),
+  tv_S_alphas = c(0.5, 3),
+  tv_S_gammas = c(0.3, 1),
+  tv_S_thetas = c(0.3, 0.8),
+  search_S_alphas = c(0.5, 3),
+  search_S_gammas = c(0.3, 1),
+  search_S_thetas = c(0, 0.3),
+  ooh_S_alphas = c(0.5, 3),
+  ooh_S_gammas = c(0.3, 1),
+  ooh_S_thetas = c(0.1, 0.4),
+  newsletter_alphas = c(0.5, 3),
+  newsletter_gammas = c(0.3, 1),
+  newsletter_thetas = c(0.1, 0.4)
+)
+
+# Example hyperparameters ranges for Weibull CDF adstock
+# facebook_S_alphas = c(0.5, 3)
+# facebook_S_gammas = c(0.3, 1)
+# facebook_S_shapes = c(0.0001, 2)
+# facebook_S_scales = c(0, 0.1)
+
+# Example hyperparameters ranges for Weibull PDF adstock
+# facebook_S_alphas = c(0.5, 3
+# facebook_S_gammas = c(0.3, 1)
+# facebook_S_shapes = c(0.0001, 10)
+# facebook_S_scales = c(0, 0.1)
+
+#### 2a-3: Third, add hyperparameters into robyn_inputs()
+
+InputCollect <- robyn_inputs(InputCollect = InputCollect, hyperparameters = hyperparameters)
+print(InputCollect)
+
+# #### Check spend exposure fit if available
+# if (length(InputCollect$exposure_vars) > 0) {
+#   InputCollect$modNLS$plots$facebook_I
+#   InputCollect$modNLS$plots$search_clicks_P
+# }
+
+################################################################
+#### Step 3: Build initial model
+
+## Run all trials and iterations. Use ?robyn_run to check parameter definition
+OutputModels <- robyn_run(
+  InputCollect = InputCollect, # feed in all model specification
+  cores = 5, # default to max available
+  # add_penalty_factor = FALSE, # Untested feature. Use with caution.
+  iterations = 2000, # recommended for the dummy dataset
+  trials = 1, # recommended for the dummy dataset
+  outputs = FALSE # outputs = FALSE disables direct model output - robyn_outputs()
+)
+print(OutputModels)
+
+## Check MOO (multi-objective optimization) convergence plots
+OutputModels$convergence$moo_distrb_plot
+OutputModels$convergence$moo_cloud_plot
+# check convergence rules ?robyn_converge
+
+## Calculate Pareto optimality, cluster and export results and plots. See ?robyn_outputs
+OutputCollect <- robyn_outputs(
+  InputCollect, OutputModels,
+  pareto_fronts = 3,
+  # calibration_constraint = 0.1, # range c(0.01, 0.1) & default at 0.1
+  csv_out = "pareto", # "pareto" or "all"
+  clusters = TRUE, # Set to TRUE to cluster similar models by ROAS. See ?robyn_clusters
+  plot_pareto = TRUE, # Set to FALSE to deactivate plotting and saving model one-pagers
+  plot_folder = robyn_object # path for plots export
+)
+print(OutputCollect)
+
+## 4 csv files are exported into the folder for further usage. Check schema here:
+## https://github.com/facebookexperimental/Robyn/blob/main/demo/schema.R
+# pareto_hyperparameters.csv, hyperparameters per Pareto output model
+# pareto_aggregated.csv, aggregated decomposition per independent variable of all Pareto output
+# pareto_media_transform_matrix.csv, all media transformation vectors
+# pareto_alldecomp_matrix.csv, all decomposition vectors of independent variables
